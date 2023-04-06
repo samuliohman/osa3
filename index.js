f21781b4e92b77ce5a4ef95b052a5047be93cdcd
Samuli Öhman <samuli.ohman@aalto.fi>
2023-04-06 14:46:19 +0300

    Tehtävä 3.4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+var persons = [
     {
         id: 1,
         name: 'Arto Hellas',
@@ -38,6 +38,15 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    if (!persons.map(p => p.id).includes(id)) {
+        return response.status(404).end()
+    }
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
 })
 
 const PORT = 3001
